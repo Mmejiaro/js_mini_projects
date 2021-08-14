c5ee3307c7c76f47d498029c14e7f35ae2dd1298
Michael Mejia <michaelmejia1171@gmail.com>
2021-08-14 16:39:56 -0400

    added functionlity to the video (Play, Pause & Stop)

--- custom-video-player/script.js
+++ custom-video-player/script.js
@@ -1,3 +1,52 @@
 window.onload = function () {
+    const video = document.getElementById('video');
+    const play = document.getElementById('play');
+    const stop = document.getElementById('stop');
+    const progress = document.getElementById('progress');
+    const timestamp = document.getElementById('timestamp');
 
+    // Event Listeners
+    video.addEventListener('click', toggleVideoStatus);
+    video.addEventListener('pause', updatePlayIcon);
+    video.addEventListener('play', updatePlayIcon);
+    video.addEventListener('timeupdate', updateProgress);
+
+    play.addEventListener('click', toggleVideoStatus);
+    stop.addEventListener('click', stopVideo);
+    progress.addEventListener('change', setVideoProgress);
+
+    // Play and pause video
+    function toggleVideoStatus() {
+        if (video.paused) {
+            video.play();
+        }
+        else {
+            video.pause();
+        }
+    }
+
+    // update the play/pause icon
+    function updatePlayIcon() {
+        if (video.paused) {
+            play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+        }
+        else {
+            play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+        }
+    }
+
+    // update progress and timestamp
+    function updateProgress() {
+        return true;
+    }
+
+    // Set video time to progress
+    function setVideoProgress() {
+        return true;
+    }
+
+    function stopVideo() {
+        video.currentTime = 0;
+        video.pause();
+    }
 }
